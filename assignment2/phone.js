50b557e70026b5df6938682ce7faa0cb942c434a
kri111 <krishna.adettiwar@colorado.edu>
2018-02-14 18:56:03 -0700

    working buttons with input field updating

--- assignment2/phone.js
+++ assignment2/phone.js
@@ -22,7 +22,54 @@ $("#button_add").click(function(){ //when "button_id" is clicked
   $("#content_list").hide();
 });
 
-$("#button_1").click(function(){ //when "button_id" is clicked
-  $("#dialer_entry").val($("#dialer_entry").val() + "1")
-  alert($("#dialer_entry").val());
+$("#button_1").click(function(){
+  $("#dialer_entry").val($("#dialer_entry").val() + "1");
+});
+
+$("#button_2").click(function(){
+  $("#dialer_entry").val($("#dialer_entry").val() + "2");
+});
+
+$("#button_3").click(function(){
+  $("#dialer_entry").val($("#dialer_entry").val() + "3");
+});
+
+$("#button_4").click(function(){
+  $("#dialer_entry").val($("#dialer_entry").val() + "4");
+});
+
+$("#button_5").click(function(){
+  $("#dialer_entry").val($("#dialer_entry").val() + "5");
+});
+
+$("#button_6").click(function(){
+  $("#dialer_entry").val($("#dialer_entry").val() + "6");
+});
+
+$("#button_7").click(function(){
+  $("#dialer_entry").val($("#dialer_entry").val() + "7");
+});
+
+$("#button_8").click(function(){
+  $("#dialer_entry").val($("#dialer_entry").val() + "8");
+});
+
+$("#button_9").click(function(){
+  $("#dialer_entry").val($("#dialer_entry").val() + "9");
+});
+
+$("#button_0").click(function(){
+  $("#dialer_entry").val($("#dialer_entry").val() + "0");
+});
+
+$("#button_star").click(function(){
+  $("#dialer_entry").val($("#dialer_entry").val() + "*");
+});
+
+$("#button_pound").click(function(){
+  $("#dialer_entry").val($("#dialer_entry").val() + "#");
+});
+
+$("#button_clear").click(function(){
+  $("#dialer_entry").val("");
 });
